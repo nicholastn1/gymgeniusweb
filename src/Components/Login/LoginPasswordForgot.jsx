dd812310f245b23279e13733b026f910691225b0
nicholastn1 <nicholastimbo2022@gmail.com>
2023-05-30 15:12:16 -0300

    Creating password forgot and reset password pages

--- src/Components/Login/LoginPasswordForgot.jsx
+++ src/Components/Login/LoginPasswordForgot.jsx
@@ -3,37 +3,44 @@ import Input from '../Forms/Input';
 import Button from '../Forms/Button';
 import useForm from '../../Hooks/useForm';
 import useFetch from '../../Hooks/useFetch';
+import { FORGOT_PASSWORD } from '../../api';
+import Error from '../Helper/Error';
 
 const LoginPasswordForgot = () => {
-  const email = useForm();
+  const user = useForm();
   const { data, loading, error, request } = useFetch();
+  console.log(error);
 
-  function handleSubmit(event) {
+  async function handleSubmit(event) {
     event.preventDefault();
 
-    if (email.validate()) {
-      const { url, options } = {
-        url: '/api/password/forgot',
-        options: {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(email.value),
-        },
-      };
+    if (user.validate()) {
+      const { url, options } = FORGOT_PASSWORD({
+        user: { email: user.value },
+      });
 
-      request(url, options);
+      const { json } = await request(url, options);
+      console.log(json);
     }
   }
 
   return (
     <section>
       <h1 className="title">Forgot your password?</h1>
-      <form onSubmit={handleSubmit}>
-        <Input label="Email" type="text" name="email" {...email} />
-        <Button>Sent to Email</Button>
-      </form>
+      {data ? (
+        <p style={{ color: '#4c1' }}>{data.message}</p>
+      ) : (
+        <form onSubmit={handleSubmit}>
+          <Input label="Email" type="text" name="user" {...user} />
+          {loading ? (
+            <Button disabled>Sending...</Button>
+          ) : (
+            <Button>Sent to Email</Button>
+          )}
+
+          {<Error error={error} />}
+        </form>
+      )}
     </section>
   );
 };
--- src/Components/Login/LoginPasswordReset.jsx
+++ src/Components/Login/LoginPasswordReset.jsx
@@ -1,7 +1,61 @@
 import React from 'react';
+import Input from '../Forms/Input';
+import Error from '../Helper/Error';
+import Button from '../Forms/Button';
+import useForm from '../../Hooks/useForm';
+import useFetch from '../../Hooks/useFetch';
+import { RESET_PASSWORD } from '../../api';
+import { useNavigate } from 'react-router-dom';
 
 const LoginPasswordReset = () => {
-  return <div>Login Password Reset</div>;
+  const [email, setEmail] = React.useState();
+  const [token, setToken] = React.useState();
+  const password = useForm('password');
+  const { error, loading, request } = useFetch();
+  const navigate = useNavigate();
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    if (password.validate()) {
+      const { url, options } = RESET_PASSWORD({
+        user: {
+          email: email,
+          token: token,
+          password: password.value,
+        },
+      });
+      const { response } = await request(url, options);
+      if (response.ok) navigate('/login');
+    }
+  }
+
+  React.useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    const token = params.get('reset_password_token');
+    const email = params.get('email');
+    if (token) setToken(token);
+    if (email) setEmail(email);
+  }, []);
+
+  return (
+    <div>
+      <h1 className="title">Reset Password</h1>
+      <form onSubmit={handleSubmit}>
+        <Input
+          label="New Password"
+          type="password"
+          name="password"
+          {...password}
+        />
+        {loading ? (
+          <Button disabled>Resetting...</Button>
+        ) : (
+          <Button>Reset Password</Button>
+        )}
+        <Error error={error} />
+      </form>
+    </div>
+  );
 };
 
 export default LoginPasswordReset;
